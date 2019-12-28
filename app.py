25d2183d68ba48fe2f667fd6dfb0154f5a722f3d
morgan8514 <morgangicheha4@gmail.com>
2019-12-28 15:47:28 +0300

    second commit

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,flash
 from twilio.rest import Client 
+from models.models import Sending
+import random
 
 app  = Flask(__name__)
 
-# twillio credentials
-# encrypted username/number
-account_sid = "AC*******************"
-# password
-auth_token = "71********************"
-print("auths recievd from twilio")
-client = Client(account_sid, auth_token)
+# configuring secret key
+app.config['SECRET_KEY']= random.random()
 
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
         reciever=request.form['reciever']
         content_to_send=request.form['content_to_send']
-        recieverr=reciever
+
+        # calling the sending class
+        try:
+            send= Sending(body=content_to_send,to=reciever)
+            send.send_sms()
+            flash('Message sent .','info')
+        except Exception:
+           flash('An error occured! Retry','Warning')
+           
+            
 
 
     return render_template('home.html')
--- models/models.py
+++ models/models.py
@@ -1,7 +1,34 @@
+from twilio.rest import Client
+
+
 class Sending:
-    def __init__(self,to, from_,message):
-        self.to=to
-        self.from_=from_
-        self.message=message
+    ''' this model is for sending sms with twilio'''
+    from_='+1562*******'
+    
+    account_sid = 'AC2337805f2c4adaf746eb*********'
+    auth_token = '40c*****************************'
+      
+    def __init__(self,body,to):
+            self.body=body
+            self.to=to
+
+    '''verification function. it verifies twilio credentials'''
+    def verify(self):
+        pass
+
+    def send_sms(self):
+        
+        client=Client(self.account_sid,self.auth_token)
+        print('auths recieved')
+        message= client.messages.create(self.to ,from_=self.from_,body=self.body)
+        print('message sent')
+
+
+
+
+
+
+
+
         
     
